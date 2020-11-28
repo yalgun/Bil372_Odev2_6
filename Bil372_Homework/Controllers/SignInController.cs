65a51ca721b09fa8d80fcc51fc22aff8d1f468d3
yalgun <43279137+yalgun@users.noreply.github.com>
2020-11-29 02:17:15 +0300

    Login and SignIn done.

--- Bil372_Homework/Controllers/SignInController.cs
+++ Bil372_Homework/Controllers/SignInController.cs
@@ -9,33 +9,23 @@ using System.Web.Mvc;
 
 namespace Bil372_Homework.Controllers
 {
-    public class UserController : Controller
+    public class SignInController : Controller
     {
         SqlConnection con = new SqlConnection();
         SqlCommand com = new SqlCommand();
         SqlDataReader dr;
-
-        private AppDbContext dbContext;
-        public UserController()
+        AppDbContext  dbContext;
+        // GET: SignIn
+        public SignInController()
         {
             dbContext = new AppDbContext("Server=tcp:databaseadmin.database.windows.net,1433;Initial Catalog=Bil372Odev;Persist Security Info=False;User ID=databaseadmin;Password=admindatabase_9;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         }
 
-        // GET: User
-        [HttpGet]
-        public ActionResult Login()
-        {
-            var viewModel = new UserViewModel();
-            viewModel.Users = dbContext.Users.ToList();
-            return View(viewModel);
-        }
-
-        public ActionResult Index()
+        public ActionResult SignIn()
         {
             return View();
         }
-
-        public ActionResult SignIn()
+        public ActionResult Index()
         {
             return View();
         }
@@ -43,26 +33,6 @@ namespace Bil372_Homework.Controllers
         void connectionString()
         {
             con.ConnectionString = "Server=tcp:databaseadmin.database.windows.net,1433;Initial Catalog=Bil372Odev;Persist Security Info=False;User ID=databaseadmin;Password=admindatabase_9;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        }
-
-        [HttpPost]
-        public ActionResult Verify(User user)
-        {
-            connectionString();
-            con.Open();
-            com.Connection = con;
-            com.CommandText = "SELECT * FROM [dbo].[User] WHERE UserName ='" + user.UserName + "' and Password ='" + user.Password + "'";
-            dr = com.ExecuteReader();
-            if (dr.Read())
-            {
-                con.Close();
-                return View("Success");
-            }
-            else
-            {
-                con.Close();
-                return View("Error");
-            }
         }
 
         [HttpPost]
---
+++ Bil372_Homework/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Bil372_Homework.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Bil372_Homework.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(string connectionString) : base(connectionString)
+        {
+        }
+
+        public IDbSet<User> Users { get; set; }
+        public IDbSet<UserInfo> UserInfos { get; set; }
+        public IDbSet<UserLog> UserLogs { get; set; }
+        public IDbSet<Submission> Submissions { get; set; }
+        public IDbSet<Conference> Conferences { get; set; }
+        public IDbSet<ConferenceRoles> ConferenceRoles { get; set; }
+
+    }
+}
