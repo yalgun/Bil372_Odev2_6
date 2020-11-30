dfa7194e6d9517df7d661e3b9da4f91c026a5f45
yalgun <43279137+yalgun@users.noreply.github.com>
2020-12-01 02:49:44 +0300

    Database Model Updated.

--- Bil372_Homework/Models/ConferenceRoles.cs
+++ Bil372_Homework/Models/ConferenceRoles.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,7 +11,11 @@ namespace Bil372_Homework.Models
     {
         public int ConferenceRole { get; set; }
         [Key]
+        [Column(Order = 0)]
         public int AuthenticationID { get; set; }
+        [Key]
+        [Column(Order = 1)]
+        public int ConfID { get; set; }
     }
 
     public class ConferenceRolesViewModel
