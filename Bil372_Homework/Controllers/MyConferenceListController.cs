8d6fd2e0b6b005e2e35a6243e88aa769ce331686
rabiayunusoglu <ryunusoglu@etu.edu.tr>
2020-11-30 13:23:29 +0300

    update

---
+++ Bil372_Homework/Controllers/MyConferenceListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bil372_Homework.Controllers
+{
+    public class MyConferenceListController : Controller
+    {
+        // GET: MyConferenceList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
